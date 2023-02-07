6b1d50fb0760f3bba9436b5651caada3dc16f61f
norld <norld@norld.com>
2023-02-07 10:32:43 +0800

    添加模块-偶像大师闪耀色彩汉化

---
+++ module/ShinyColors/shiny_colors_fy.js
@@ -0,0 +1,29 @@
+function insert(str, index, value) {
+  return str.substr(0, index) + value + str.substr(index);
+}
+
+const toInsert = `
+<script>
+(function () {
+  const script = document.createElement("script");
+  script.src = "https://www.shiny.fun/ShinyColors.user.js";
+  document.head.appendChild(script);
+})();
+</script>
+`;
+
+let { body, headers } = $response;
+const result = {};
+
+if (
+  body &&
+  headers &&
+  typeof headers["Content-Type"] === "string" &&
+  headers["Content-Type"].includes("text/html") &&
+  body.lastIndexOf("</head>") !== -1
+) {
+  result.body = insert(body, body.lastIndexOf("</head>"), toInsert);
+  result.headers = headers;
+}
+
+$done(result);
